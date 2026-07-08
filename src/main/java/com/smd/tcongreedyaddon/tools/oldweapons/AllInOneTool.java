d6ee94e7683ebac9a6cb63726647807fd1e8d870
smd <88483238+x8vxv8x@users.noreply.github.com>
2026-07-08 12:30:48 +0800

    全能工具改进
    1.完全禁止造成伤害
    2.修复作为铲子使用时,无法获得挖掘速度加成的问题
    3.修复一些情况下挖掘等级异常的问题
    4.修复草径制作无效的问题

--- src/main/java/com/smd/tcongreedyaddon/tools/oldweapons/AllInOneTool.java
+++ src/main/java/com/smd/tcongreedyaddon/tools/oldweapons/AllInOneTool.java
@@ -13,13 +13,16 @@ import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.util.EnumActionResult;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.EnumHand;
+import net.minecraft.util.StringUtils;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.text.TextFormatting;
 import net.minecraft.world.World;
 
 import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
 
 import slimeknights.tconstruct.library.Util;
+import slimeknights.tconstruct.library.materials.ExtraMaterialStats;
 import slimeknights.tconstruct.library.materials.HandleMaterialStats;
 import slimeknights.tconstruct.library.materials.HeadMaterialStats;
 import slimeknights.tconstruct.library.materials.Material;
@@ -35,18 +38,41 @@ import java.util.List;
 
 public class AllInOneTool extends AoeToolCore {
 
-    public static final ImmutableSet<net.minecraft.block.material.Material> effectiveMaterials =
+    public static final ImmutableSet<net.minecraft.block.material.Material> effectiveMaterialsPick =
             ImmutableSet.of(net.minecraft.block.material.Material.ROCK,
                     net.minecraft.block.material.Material.IRON,
                     net.minecraft.block.material.Material.ANVIL,
-                    net.minecraft.block.material.Material.GLASS,
-                    net.minecraft.block.material.Material.GRASS,
-                    net.minecraft.block.material.Material.GROUND,
-                    net.minecraft.block.material.Material.CLAY,
-                    net.minecraft.block.material.Material.WOOD,
+                    net.minecraft.block.material.Material.GLASS);
+
+    public static final ImmutableSet<net.minecraft.block.material.Material> effectiveMaterialsAxe =
+            ImmutableSet.of(net.minecraft.block.material.Material.WOOD,
+                    net.minecraft.block.material.Material.CACTUS,
                     net.minecraft.block.material.Material.PLANTS,
                     net.minecraft.block.material.Material.VINE,
                     net.minecraft.block.material.Material.GOURD);
+
+    public static final ImmutableSet<net.minecraft.block.material.Material> effectiveMaterialsShovel =
+            ImmutableSet.of(net.minecraft.block.material.Material.GRASS,
+                    net.minecraft.block.material.Material.GROUND,
+                    net.minecraft.block.material.Material.SAND,
+                    net.minecraft.block.material.Material.CRAFTED_SNOW,
+                    net.minecraft.block.material.Material.SNOW,
+                    net.minecraft.block.material.Material.CLAY,
+                    net.minecraft.block.material.Material.CAKE);
+
+    public static final ImmutableSet<Block> effectiveBlocksShovel =
+            ImmutableSet.of(Blocks.CLAY,
+                    Blocks.DIRT,
+                    Blocks.FARMLAND,
+                    Blocks.GRASS,
+                    Blocks.GRASS_PATH,
+                    Blocks.GRAVEL,
+                    Blocks.MYCELIUM,
+                    Blocks.SAND,
+                    Blocks.SNOW,
+                    Blocks.SNOW_LAYER,
+                    Blocks.SOUL_SAND,
+                    Blocks.CONCRETE_POWDER);
 
     public AllInOneTool() {
         super(PartMaterialType.handle(TinkerTools.toughToolRod),
@@ -65,7 +91,10 @@ public class AllInOneTool extends AoeToolCore {
 
     @Override
     public boolean isEffective(IBlockState state) {
-        return effectiveMaterials.contains(state.getMaterial());
+        return effectiveMaterialsPick.contains(state.getMaterial())
+                || effectiveMaterialsAxe.contains(state.getMaterial())
+                || effectiveMaterialsShovel.contains(state.getMaterial())
+                || effectiveBlocksShovel.contains(state.getBlock());
     }
 
     @Override
@@ -80,7 +109,7 @@ public class AllInOneTool extends AoeToolCore {
 
     @Override
     public double attackSpeed() {
-        return 0.1f;
+        return 1f;
     }
 
     @Override
@@ -90,28 +119,40 @@ public class AllInOneTool extends AoeToolCore {
 
     @Override
     public boolean dealDamage(ItemStack stack, EntityLivingBase player, Entity entity, float damage) {
+        return false;
+    }
 
-        return super.dealDamage(stack, player, entity, damage * 1/100);
+    @Override
+    public boolean onLeftClickEntity(ItemStack stack, EntityPlayer player, Entity entity) {
+        return true;
     }
 
     @Nonnull
     @Override
     public EnumActionResult onItemUse(EntityPlayer player, World world, BlockPos pos, EnumHand hand,
                                       EnumFacing facing, float hitX, float hitY, float hitZ) {
-        IBlockState state = world.getBlockState(pos);
-        Block block = state.getBlock();
-
-        if (block == Blocks.GRASS || block == Blocks.DIRT) {
-            world.setBlockState(pos, Blocks.FARMLAND.getDefaultState());
-            return EnumActionResult.SUCCESS;
+        if (player.isSneaking()) {
+            return this.doMakePath(player, world, pos, hand, facing, hitX, hitY, hitZ);
         }
+        return this.doTill(player, world, pos, hand, facing, hitX, hitY, hitZ);
+    }
 
-        if (player.isSneaking() && block == Blocks.GRASS) {
-            world.setBlockState(pos, Blocks.GRASS_PATH.getDefaultState());
-            return EnumActionResult.SUCCESS;
+    @Override
+    public int getHarvestLevel(ItemStack stack, String toolClass, @Nullable EntityPlayer player,
+                               @Nullable IBlockState blockState) {
+        if (StringUtils.isNullOrEmpty(toolClass)) {
+            return -1;
         }
-
-        return EnumActionResult.PASS;
+        if (toolClass.equals("pickaxe")) {
+            return this.getPickLevel(stack);
+        }
+        if (toolClass.equals("axe")) {
+            return this.getAxeLevel(stack);
+        }
+        if (toolClass.equals("shovel")) {
+            return this.getShovelLevel(stack);
+        }
+        return super.getHarvestLevel(stack, toolClass, player, blockState);
     }
 
     protected int getPickLevel(ItemStack stack) {
@@ -142,7 +183,6 @@ public class AllInOneTool extends AoeToolCore {
         info.add(String.format("%s: %s", text, HarvestLevels.getHarvestLevelName(getShovelLevel(stack))) + TextFormatting.RESET);
 
         info.addMiningSpeed();
-        info.addAttack();
 
         if (ToolHelper.getFreeModifiers(stack) > 0) {
             info.addFreeModifiers();
@@ -163,11 +203,14 @@ public class AllInOneTool extends AoeToolCore {
         HeadMaterialStats pick = materials.get(1).getStatsOrUnknown(MaterialTypes.HEAD);
         HeadMaterialStats axe = materials.get(2).getStatsOrUnknown(MaterialTypes.HEAD);
         HeadMaterialStats shovel = materials.get(3).getStatsOrUnknown(MaterialTypes.HEAD);
+        ExtraMaterialStats core = materials.get(4).getStatsOrUnknown(MaterialTypes.EXTRA);
 
         AllInOneToolNBT data = new AllInOneToolNBT();
         data.head(pick, axe, shovel);
         data.handle(handle);
-        data.durability *= 0.8;
+        data.extra(core);
+        data.durability = Math.max(1, Math.round(data.durability * 0.8f));
+        data.attack = 1.0f;
 
         data.pickLevel = pick.harvestLevel;
         data.axeLevel = axe.harvestLevel;
