caf7c7196ddef1e0fbd5e4acdaf9af1c27e4d14d
smd <88483238+x8vxv8x@users.noreply.github.com>
2026-07-09 10:35:26 +0800

    防止渔竿能钩住钓手本身

--- src/main/java/com/smd/tcongreedyaddon/mixin/vanilla/MixinEntityFishHook.java
+++ src/main/java/com/smd/tcongreedyaddon/mixin/vanilla/MixinEntityFishHook.java
@@ -7,6 +7,7 @@ import net.minecraft.entity.projectile.EntityFishHook;
 import net.minecraft.item.ItemFishingRod;
 import net.minecraft.item.ItemStack;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -14,6 +15,9 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(EntityFishHook.class)
 public abstract class MixinEntityFishHook {
+
+    @Shadow
+    private EntityPlayer angler;
 
     @Inject(method = "shouldStopFishing", at = @At("HEAD"), cancellable = true)
     private void tcongreedyaddon$allowTinkerFishingRod(CallbackInfoReturnable<Boolean> cir) {
@@ -64,6 +68,15 @@ public abstract class MixinEntityFishHook {
 
         if (!hook.world.isRemote && hook.caughtEntity != null) {
             FishingRodHooks.recordHitSpeed(hook);
+        }
+    }
+
+    @Inject(method = "canBeHooked", at = @At("HEAD"), cancellable = true)
+    private void tcongreedyaddon$preventTinkerRodHookingSelf(Entity entity, CallbackInfoReturnable<Boolean> cir) {
+        if (entity == this.angler
+                && this.angler != null
+                && !FishingRodHooks.findRod(this.angler).isEmpty()) {
+            cir.setReturnValue(false);
         }
     }
 
